3dccd7a2aaa03e11697fb25abf44223eacafad7a
zhoutao <zhoutao_233@163.com>
2022-04-27 21:35:56 +0800

    chore: init blog

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,9 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  base: '/blog/',
+  lang: 'zh-CN',
+  title: 'Blog',
+  description: '一个简单的博客',
+  // head: [['link', { rel: 'icon', href: '/images/logo.png' }]]
+})
